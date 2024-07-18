455442f30503a2d2d2a20d045108c6a71c300290
NadyaEvp <t_evp@mail.ru>
2024-07-18 14:28:56 +0300

    Настраиваем пробелы

--- datacenter/general_functions.py
+++ datacenter/general_functions.py
@@ -1,18 +1,18 @@
 from django.utils.timezone import localtime
 
 def get_duration(visit):
-  time_entered = localtime(visit.entered_at)
-  time_leaved = localtime(visit.leaved_at)
-  time = time_leaved - time_entered
-  return time.total_seconds()
+    time_entered = localtime(visit.entered_at)
+    time_leaved = localtime(visit.leaved_at)
+    time = time_leaved - time_entered
+    return time.total_seconds()
 
 def format_duration(duration):
-  hours = int(duration // 3600)
-  minutes= int((duration % 3600) // 60)
-  return f'{hours}:{minutes}'
+    hours = int(duration // 3600)
+    minutes= int((duration % 3600) // 60)
+    return f'{hours}:{minutes}'
 
 def is_visit_long(visit):
-  hour = 60
-  seconds = get_duration(visit)
-  time = int(seconds // hour)
-  return time >= hour
+    hour = 60
+    seconds = get_duration(visit)
+    time = int(seconds // hour)
+    return time >= hour
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,14 +9,14 @@ def storage_information_view(request):
     visits = Visit.objects.filter(leaved_at=None)
     
     for visit in visits:
-          duration= format_duration(get_duration(visit))
-          visit = {
-              'who_entered': visit.passcard.owner_name,
-              'entered_at': localtime(visit.entered_at),
-              'duration': duration,
-              'is_strange':  is_visit_long(visit)
-          }
-          non_closed_visits.append(visit)  
+        duration= format_duration(get_duration(visit))
+        visit = {
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': localtime(visit.entered_at),
+            'duration': duration,
+            'is_strange':  is_visit_long(visit)
+        }
+        non_closed_visits.append(visit)  
         
     context = {
         'non_closed_visits': non_closed_visits,
